442ce478246f94f7f7daccb03ce3b3ab51e5b9c7
Parv17k <khatriparv@gmail.com>
2020-11-29 23:52:27 -0500

    database schema done;temp login script;temp dashboard

--- main.py
+++ main.py
@@ -3,38 +3,47 @@ import streamlit as st
 import psycopg2 
 ##import plotly.express as px
 ##import SessionState
-def auth(email,password):
-    if email == password:
-        return True
-    else:
-        return -1
+userInfo={}
+def auth(id,password,domain):
+    con=createConnection()
+    cursor= con.cursor()
+    cursor.execute("SELECT * from "+domain+" where id="+id+" and password="+password";");
+    record = cursor.fetchone()
+    st.write("You are  - ", record);
+    st.write();
+      return record;
+
+
 @st.cache(allow_output_mutation=True)
 def createConnection():
+    try:
+        conn = psycopg2.connect(host='suleiman.db.elephantsql.com',
+                        port='5432',
+                        database='ncteubln',
+                        user= 'ncteubln',
+                        password= '2GVWH57HylcCfWVlSE2Z1NqQ0-FklkM7')
+        return conn;
+    except(Exception, psycopg2.Error) as error:
+        print("Error connecting to PostgreSQL database", error)
+    return None
 
 
-    conn = psycopg2.connect(host='suleiman.db.elephantsql.com',
-                      port='5432',
-                      database='ncteubln',
-                      user= 'ncteubln',
-                      password= '2GVWH57HylcCfWVlSE2Z1NqQ0-FklkM7')
-    #st.write('jvndbnmvcb',conn)
-    return conn
-@st.cache
-def get_data():
-    url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv"
-    return pd.read_csv(url)
-df = get_data()
 st.title("Issue Management System")
-option = st.selectbox('Select your domain/role?',('Reporter', 'Management', 'Employee'))
+option = st.selectbox('Select your domain/role?',('employees', 'issue_reporter', 'chief_management'))
 st.write('Your selected domian:', option)
-conn = createConnection()
-st.write('jvndbnmvcb',conn)
-user_email = st.text_input("Email:")
+user_id = st.text_input("ID:")
 user_password = st.text_input("Password:")
 if st.button('add'):
-    result = auth(user_email, user_password)
+    result = auth(user_id, user_password,option)
     if result != -1:
         st.write("Welcome user")
     else:
         st.write("Please check mentioned credentials!")
 
+
+def dashboard_reporter:
+    st.write("Hello Issue Reporter");
+def dashboard_employee:
+    st.write("Hello Issue Reporter");
+def dashboard_management:
+    st.write("Hello Issue Reporter"); 
