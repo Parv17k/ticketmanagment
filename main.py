a70f4b473243b8b3c91e7bb7c2475afe5eaaba0f
Parv17k <khatriparv@gmail.com>
2020-12-04 20:06:47 -0500

    Dashboad tables for All users

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import SessionState
 session_state = SessionState.get(id=0,name='Parv')
 head=st.title("")
 
+
 def get_config(filename='database.ini', section='postgresql'):
     parser = ConfigParser()
     parser.read(filename)
@@ -15,6 +16,7 @@ def get_config(filename='database.ini', section='postgresql'):
 @st.cache
 def query_db(sql: str):
     # print(f'Running query_db(): {sql}')
+    conn=None
     try:
         db_info = get_config()
 
@@ -24,25 +26,33 @@ def query_db(sql: str):
         conn = createConnection()
     # Open a cursor to perform database operations
     cur = conn.cursor()
+    try:
+        
+       # Execute a command: this creates a new table
+        cur.execute(sql)
 
-    # Execute a command: this creates a new table
-    cur.execute(sql)
+        # Obtain data
+        data = cur.fetchall()
+        
+        column_names = [desc[0] for desc in cur.description]
 
-    # Obtain data
-    data = cur.fetchall()
+        # Make the changes to the database persistent
+        conn.commit()
+
+        # Close communication with the database
+    # cur.close()
+        #conn.close()
+
+        df = pd.DataFrame(data=data, columns=column_names)
+        return df
+
+    except Exception as error:
+        cur.execute("ROLLBACK")
+        conn.commit()
+        st.write("Error Occured",error)
+    return None
+
     
-    column_names = [desc[0] for desc in cur.description]
-
-    # Make the changes to the database persistent
-    conn.commit()
-
-    # Close communication with the database
-   # cur.close()
-    #conn.close()
-
-    df = pd.DataFrame(data=data, columns=column_names)
-
-    return df
 
 @st.cache(allow_output_mutation=True)
 def createConnection():
@@ -60,25 +70,43 @@ def createConnection():
 @st.cache
 def load_tickets(user):
     sql=''
+    print("You are ",user)
     if user['domain'][0]=='issue_reporter':
-        sql = 'select  * from tickets where issuer_id = '+str(user['id'][0])
+        
+        sql = """select i.name, et.ticket_id, et.start_time, et.end_time, et.title, et.issuer_id, et.assigned_to_id, et.name as emp_name from
+         (select  *
+        from tickets t left join employees  e on issuer_id = """+str(user['id'][0])+""")
+         et left join issue_reporter i on i.id = et.issuer_id;"""
     elif user['domain'][0]=='employees':
-        sql = 'select  * from tickets where assigned_to_id = '+str(user['id'][0])
+        sql = """select i.name, et.ticket_id, et.start_time, et.end_time, et.title, et.issuer_id, et.assigned_to_id, et.name as emp_name from
+         (select  *
+        from tickets t left join employees  e on e.id ="""+str(user['id'][0])+""" )
+         et left join issue_reporter i on i.id = et.issuer_id;"""
+ 
+    else:
+        sql= """
+        select temp.title, m.name as manager_name , temp.name as management_system,temp.type as department
+        from ( select  t.title, ms.name,t.ms_id,ms.type from tickets t right join management_system ms on t.ms_id=ms.id) temp ,chief_management m
+        where m.id = temp.ms_id  and  m.id = """+str(user['id'][0])+""" ;"""
+        print(sql)
     data=query_db(sql)
+    print(data)
     return data
+
 def show_tickets(user):
     st.table(load_tickets(user))
 
 
 def dashboard_reporter(user_data):
     head.title("Issue reporter Dashboard")
-    st.success("Hello Issue Reporter"+", Please scroll down and access your dashboard")
+    st.success("Hello Issue Reporter '"+user_data['name'][0]+"', Please scroll down and access your dashboard")
     show_tickets(user_data)
 def dashboard_employee(user_data):
-     st.success("Hello"+", Please scroll down and access your dashboard")
+     st.success("Hello '"+user_data['name'][0]+"', Please scroll down and access your dashboard")
      show_tickets(user_data)
 def dashboard_management(user_data):
-     st.success("Hello Issue Reporter"+", Please scroll down and access your dashboard")
+     st.success("Hello '"+user_data['name'][0]+"', Please scroll down and access your dashboard")
+     show_tickets(user_data)
 def auth(id,password,domain):
 
     query="SELECT * from "+domain+" where id="+id+" and password = '"+password+"';"
@@ -87,8 +115,6 @@ def auth(id,password,domain):
         print(df)
         return df
     except Exception as error:
-        cursor.execute("ROLLBACK")
-        con.commit()
         st.write("Error Occured",error)
     return 
 
@@ -108,26 +134,9 @@ if st.button('add'):
         if "employees" == option:
             dashboard_employee(result)
         elif 'issue_reporter' == option:
-            dashboard_reporter(result)
-            
-        else:
+            dashboard_reporter(result) 
+        elif 'chief_management' == option:
             dashboard_management(result)
     
     else:
         st.write("Please check your Credentials!")
-
-"""
-'## Read tables'
-
-sql_all_table_names = "select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';"
-all_table_names = query_db(sql_all_table_names)['relname'].tolist()
-table_name = st.selectbox('Choose a table', all_table_names)
-if table_name:
-    f'Display the table'
-    sql_table = f'select * from {table_name};'
-    df = query_db(sql_table)
-    st.dataframe(df)
-
-st.write(session_state.name)
-st.write("We are here")
-"""
